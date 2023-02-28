31d7c2004e53062e989c55a4cff02694d7645363
lixuefeng <15297667215@163.com>
2023-02-28 14:23:22 +0800

    注释补充

--- src/utils/useModel.ts
+++ src/utils/useModel.ts
@@ -2,18 +2,19 @@ import { computed, ref, watch, UnwrapRef } from "vue"
 // 泛型T 一般就是props里面的属性
 export default function useModel<T>( getter: () => T, emiter: (val: T) => void  ){
   let state = ref<T>(getter())
+  // 是为了赋值层次里面的值 引起值变化
   watch(getter, (val: any ) => {
-    if (val == state.value) { 
+    if (val == state.value) {
       return
     }
-    state.value = val 
+    state.value = val
   })
   return computed({
     get(){
       return state.value
     },
     set(val: any) {
-      if (val == state.value) { 
+      if (val == state.value) {
         return
       }
       state.value = val
