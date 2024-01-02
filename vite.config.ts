c12ae7c995312f61f06a6c6af3c67ea6f882ea35
lixuefeng <15297667215@163.com>
2024-01-02 14:50:11 +0800

    feat: 库打包模式

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/package/visual-editor/index.tsx'),
+      name: 'Bundle',
+      fileName: 'bundle'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   plugins: [vue(), vueJsx()],
 })
