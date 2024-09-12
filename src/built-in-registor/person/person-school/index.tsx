86dd060cf25d6fc6bbce4c544713a3aa20ee7e84
李 雪峰 <15297667215@163.com>
2024-09-12 16:01:53 +0800

    feat: 所在中学组件完善

--- src/built-in-registor/person/person-school/index.tsx
+++ src/built-in-registor/person/person-school/index.tsx
@@ -1,51 +1,59 @@
+import { createVNode } from "vue"
 import { VisualEditorComponent } from "../../../types/editor"
+import render from "../../utils/base-input-render.vue"
+import control from "./control.vue"
 export default {
   group: "个人信息",
-  name: "textarea",
-  icon: "https://ysys-assets.oss-cn-beijing.aliyuncs.com/public/172602124292989d8172602124292946806_5.png",
+  name: "personSchool",
+  icon: "https://ysys-assets.oss-cn-beijing.aliyuncs.com/public/1726020993661913c172602099366229853_1.png",
   activeIcon:
-    "https://ysys-assets.oss-cn-beijing.aliyuncs.com/public/172602124292989d8172602124292946806_5.png",
+    "https://ysys-assets.oss-cn-beijing.aliyuncs.com/public/1726020993661913c172602099366229853_1.png",
   render: (block) => {
-    return <textarea name="" id="" cols="30" rows="10"></textarea>
+    let props = block.props
+    return createVNode(render, {
+      style: {},
+      title: props.title.defaultValue,
+      value: props.value.defaultValue,
+      placeholder: props.placeholder.defaultValue,
+      isRequire: props.isRequire.defaultValue == "1" ? true : false,
+      min: props.min.defaultValue * 1,
+      max: props.max.defaultValue * 1,
+    })
   },
   priview: () => <textarea placeholder="请输入内容"></textarea>,
   label: "学校",
-  props: {},
+  props: {
+    title: {
+      label: "标题",
+      type: "input",
+      defaultValue: "所在中学",
+    },
+    value: {
+      label: "默认文本",
+      type: "input",
+      defaultValue: "",
+    },
+    placeholder: {
+      label: "提示",
+      type: "input",
+      defaultValue: "请输入所在中学",
+    },
+    isRequire: {
+      label: "设为必填",
+      defaultValue: "2",
+    },
+    min: {
+      label: "最少填写",
+      defaultValue: 2,
+    },
+    max: {
+      label: "最多填写",
+      defaultValue: 100,
+    },
+  },
   controlView: (block, updateBlock) => {
-    let props = block.props
-    function onChange(e: any) {
-      block.props.fontSize = e.target.value
-      updateBlock(block)
-    }
-    return (
-      <div>
-        <div>
-          <span>字體大小</span>
-          <input type="number" value={props.fontSize} onChange={onChange} />
-        </div>
-        <div>
-          <span>文本内容</span>
-          <input
-            type="text"
-            v-model={props.text}
-            onChange={(e: any) => {
-              block.props.text = e.target.value
-              updateBlock(block)
-            }}
-          />
-        </div>
-        <div>
-          <span>文本颜色</span>
-          <input
-            type="text"
-            v-model={props.color}
-            onChange={(e: any) => {
-              block.props.color = e.target.value
-              updateBlock(block)
-            }}
-          />
-        </div>
-      </div>
-    )
+    return createVNode(control, {
+      block,
+    })
   },
 } as VisualEditorComponent
