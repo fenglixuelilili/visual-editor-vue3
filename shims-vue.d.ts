efd423a9c2a0cf792a92161fc1c739077d53f9d5
李 雪峰 <15297667215@163.com>
2024-09-13 18:27:24 +0800

    feat: 去除无用类型

--- shims-vue.d.ts
+++ shims-vue.d.ts
@@ -18,22 +18,6 @@ declare module '*.scss' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
-declare interface SlefWheelEvent extends WheelEvent {
-  readonly wheelDelta: number
-}
-declare interface selfHTMLElement extends HTMLElement {
-  readonly requestFullScreen: any
-  readonly webkitRequestFullScreen: any
-  readonly mozRequestFullScreen: any
-  readonly msRequestFullScreen: any
-  readonly msExitFullscreen: any
-  readonly mozCancelFullScreen: any
-  readonly webkitExitFullscreen: any
-}
-declare interface Window {
-  MathJax: any
-  myChart: any
-}
 interface Window {
   system: { [key: string]: any };
 }
