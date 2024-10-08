beaf58d5d06bebeab840e21206b11871944e0afa
李 雪峰 <15297667215@163.com>
2024-10-08 17:46:29 +0800

    fix: 省份组件更改为下拉选择的样式

--- src/built-in-registor/person/person-province/index.tsx
+++ src/built-in-registor/person/person-province/index.tsx
@@ -1,6 +1,6 @@
 import { createVNode } from "vue"
 import { VisualEditorComponent } from "../../../types/editor"
-import render from "../../utils/base-input-render.vue"
+import render from "./render.vue"
 import control from "./control.vue"
 export default {
   group: "个人信息",
@@ -16,12 +16,11 @@ export default {
     return createVNode(render, {
       style: {},
       title: props.title.defaultValue,
-      value: props.value.defaultValue,
+      // value: props.value.defaultValue,
       placeholder: props.placeholder.defaultValue,
       isRequire: props.isRequire.defaultValue == "1" ? true : false,
     })
   },
-  // priview: () => <textarea placeholder="请输入内容"></textarea>,
   label: "省份",
   props: {
     title: {
@@ -29,15 +28,15 @@ export default {
       type: "input",
       defaultValue: "所在省份",
     },
-    value: {
-      label: "默认文本",
-      type: "input",
-      defaultValue: "",
-    },
+    // value: {
+    //   label: "默认文本",
+    //   type: "input",
+    //   defaultValue: "",
+    // },
     placeholder: {
       label: "提示",
       type: "input",
-      defaultValue: "请输入所在省份",
+      defaultValue: "请选择所在省份",
     },
     isRequire: {
       label: "设为必填",
---
+++ src/built-in-registor/person/person-province/render.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="editor-built-in-commen-radio" :style="style">
+    <div class="title-tip">
+      <span class="red" v-if="isRequire">*</span>
+      <span>{{ title }}</span>
+    </div>
+    <div class="form-item" @click="tip">
+      <div class="item">
+        <span>{{ placeholder }}</span>
+        <div class="right">
+          <img
+            src="https://ysys-assets.oss-cn-beijing.aliyuncs.com/public/17262332381814dfe172623323818180331_right%20(1).png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { Message } from "@arco-design/web-vue"
+
+const props = defineProps({
+  style: {
+    type: Object,
+    default: () => ({}),
+  },
+  title: {
+    type: String,
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  isRequire: {
+    type: Boolean,
+    default: false,
+  },
+})
+function tip() {
+  Message.warning("请在移动端预览真机效果！")
+}
+</script>
+<style lang="scss" scoped>
+@import "../../commen.scss";
+.editor-built-in-commen-radio {
+  padding: 10px 24px;
+  .form-item {
+    margin-top: 8px;
+    background: #f5f7fa;
+    height: 40px;
+    border-radius: 4px;
+    cursor: pointer;
+    .item {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 12px;
+      span {
+        color: #c0c7ce;
+      }
+      .right {
+        img {
+          width: 16px;
+        }
+      }
+    }
+  }
+}
+</style>
